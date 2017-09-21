cb6c193aef9ec7edd29a1d3791c2e9c174d770a1
Sam Alsmadi <Sam.Alsmadi@gmail.com>
2017-09-20 21:30:45 -0400

    Add new subreddit routes

--- app.js
+++ app.js
@@ -3,16 +3,34 @@ var express = require("express");
 var app = express();
 
 app.get("/", function(req, res){
-    res.send("Hello there!!")
+    res.send("Hello there!!");
 });
 
 app.get("/bye", function(req, res){
-    res.send("Goodbye!!")
+    res.send("Goodbye!!");
 });
 
 app.get("/dog", function(req, res){
     console.log("Someone made a request to /dog");
     res.send("Mewo!!");
+});
+
+app.get("/r/:subredditName", function(req, res){
+    // console.log(req);
+    // console.log(req.params);
+    // res.send("Welcome to a subreddit!");
+    var subreddit = req.params.subredditName;
+    res.send("Welcome to the " + subreddit.toUpperCase() + " subreddit!");
+});
+
+app.get("/r/:subredditName/comments/:id/:title", function(req, res){
+    // console.log(req);
+    // console.log(req.params);
+    res.send("Welcome to the comments page!");
+});
+
+app.get("*", function(req, res){
+    res.send("Invalid URL!!");
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
