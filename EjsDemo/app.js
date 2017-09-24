768d134f2a1579da3cb4c4ae6422bbcc8d7c6891
Sam Alsmadi <sam.alsmadi@gmail.com>
2017-09-24 16:20:24 -0400

    Add posts route

--- EjsDemo/app.js
+++ EjsDemo/app.js
@@ -11,6 +11,15 @@ app.get("/ilove/:thing", function(req, res){
     res.render("love.ejs", {theThing: thing});
 });
 
+app.get("/posts", function(req, res){
+    var posts = [
+        {title: "Title 1", author: "Author 1"},
+        {title: "Title 2", author: "Author 2"},
+        {title: "Title 3", author: "Author 3"}
+    ];
+    res.render("posts.ejs", {posts: posts});
+});
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server has started!");
 });
