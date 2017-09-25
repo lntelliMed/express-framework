adb9fd50a202e01c6179ec07a8a5dc8ad92d0707
Sam Alsmadi <sam.alsmadi@gmail.com>
2017-09-25 00:37:39 -0400

    Add partials and stylesheet files

--- EjsDemo/app.js
+++ EjsDemo/app.js
@@ -1,14 +1,19 @@
 var express = require("express");
 var app = express();
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res){
    // res.send("<h1>Welcome to home page!</h1><h2>Bla bla bla..</h2>")
-   res.render("home.ejs");
+   // res.render("home.ejs");
+   res.render("home");
 });
 
 app.get("/ilove/:thing", function(req, res){
     var thing = req.params.thing;
-    res.render("love.ejs", {theThing: thing});
+    // res.render("love.ejs", {theThing: thing});
+    res.render("love", {theThing: thing});
 });
 
 app.get("/posts", function(req, res){
@@ -17,7 +22,9 @@ app.get("/posts", function(req, res){
         {title: "Title 2", author: "Author 2"},
         {title: "Title 3", author: "Author 3"}
     ];
-    res.render("posts.ejs", {posts: posts});
+    
+    // res.render("posts.ejs", {posts: posts});
+    res.render("posts", {posts: posts});
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
